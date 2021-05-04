8b9e7fd891ea494882591fd1d6042e1cf5eceb6f
Ilya Syrov <im.syrov.developer@yandex.ru>
2021-05-04 21:08:51 +0400

    Create a project, add and set Navigation Controller

---
+++ DiaryTestTask/DiaryTestTask/Sources/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiaryTestTask
+//
+//  Created by ilya on 04.05.2021.
+//  Copyright © 2021 ilya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
