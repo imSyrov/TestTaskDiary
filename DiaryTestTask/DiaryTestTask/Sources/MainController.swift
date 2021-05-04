af6ef59a9aeefe170d04d90985607146d3f0fd95
Ilya Syrov <im.syrov.developer@yandex.ru>
2021-05-04 21:21:07 +0400

    Delete ViewController.swift, Add UI elements into Main controller and add Outlet and action funcs

---
+++ DiaryTestTask/DiaryTestTask/Sources/MainController.swift
@@ -0,0 +1,30 @@
+//
+//  MainController.swift
+//  DiaryTestTask
+//
+//  Created by ilya on 04.05.2021.
+//  Copyright © 2021 ilya. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainController: UIViewController {
+    
+    @IBOutlet weak var calendarDatePicker: UIDatePicker!
+    @IBOutlet weak var hoursTable: UITableView!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func currentDateChange(_ sender: UIDatePicker) {
+        
+    }
+}
